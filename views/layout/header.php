9749d376a7e832f075a10b4bb214afb38d5165c5
Andre Mestanza <andremestanza971@gmail.com>
2026-08-17 23:05:50 -0500

    se agrego versionamiento

--- views/layout/header.php
+++ views/layout/header.php
@@ -39,7 +39,7 @@
                     <rect x="3" y="14" width="7" height="7"></rect>
                 </svg>
             </button>
-            <span class="header-badge" style="margin-left:0;">SG Pollada V-0.0.0.8</span>
+            <span class="header-badge" style="margin-left:0;">SG Pollada V-0.0.0.9</span>
         </div>
     </header>
 
